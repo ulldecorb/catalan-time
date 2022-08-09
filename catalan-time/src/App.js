5b875cd24db7fdf9f9f46086015d40e4b91f0360
ulldecorb <ausias.bertran@gmail.com>
2022-08-09 02:34:18 +0200

    Create converter react component Add sass css compiler

--- catalan-time/src/App.js
+++ catalan-time/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Converter from './components/converter';
 
 function App() {
+  const actualDate = {minutes: 0, hour: 2}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__header">
+        <h1 className='App__title'>CATALAN TIME</h1>
       </header>
+      <Converter actualDate={{actualDate}} />
     </div>
   );
 }
---
+++ catalan-time/src/components/converter/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// export function Converter ({actualDate}) {
+export function Converter () {
+    return (
+        <main className='converter'>
+            Cool
+        </main>
+    )
+}
+
+
+
+export default Converter;
