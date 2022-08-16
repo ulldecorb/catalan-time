bbd6e3169e0440e25e2328a26cf48fb6e210d2c7
ulldecorb <ausias.bertran@gmail.com>
2022-08-16 17:43:55 +0200

    Install eslint dependency Add eslint windows rules Refactor minor sintaxis issues

--- catalan-time/src/App.js
+++ catalan-time/src/App.js
@@ -5,53 +5,53 @@ import {
   Route
 } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/navbar';
-import Converter from './components/converter';
-import Tutorial from './components/tutorial';
-import Practice from './components/practice';
+import { Navbar } from './components/navbar';
+import { Converter } from './components/converter';
+import { Tutorial } from './components/tutorial';
+import { Practice } from './components/practice';
 
 function App() {
-  const [ initialDate, setInitialDate ] = useState('')
-  
+  const [initialDate, setInitialDate] = useState('');
+
   const setDate = () => {
     const hour = new Date().getHours();
     const minute = new Date().getMinutes();
-    setInitialDate({ hour, minute});
-  }
+    setInitialDate({ hour, minute });
+  };
 
   useEffect(() => {
-    setDate()
-  },[])
-  
-  return (    
-      <Router>
-    <div className="App">
-      <Navbar />
-      <header className="App__header">
-        <h1 className='App__title'>CATALAN TIME</h1>
-      </header>
+    setDate();
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App__header">
+          <h1 className="App__title">CATALAN TIME</h1>
+        </header>
         <Routes>
           <Route
             path="/"
             element={
               <Converter initialDate={initialDate} />
             }
-            />
+          />
           <Route
             path="/tutorial"
             element={
               <Tutorial />
             }
-            />
+          />
           <Route
             path="/practice"
             element={
               <Practice />
             }
-            />
-            </Routes>
-    </div>
-      </Router>
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- catalan-time/src/components/navbar/index.jsx
+++ catalan-time/src/components/navbar/index.jsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export function Navbar () {
-    return(
-        <header className="header">
-        <h1 className='header__title'>CATALAN TIME</h1>
-          <nav className="header__navbar">            
-          <Link
-          to={'/'}
+export function Navbar() {
+  return (
+    <header className="header">
+      <h1 className="header__title">CATALAN TIME</h1>
+      <nav className="header__navbar">
+        <Link
+          to="/"
           type="button"
           className="navbar__handler"
         >
-            HOME
+          HOME
         </Link>
-          <Link
-          to={'/tutorial'}
+        <Link
+          to="/tutorial"
           type="button"
           className="navbar__handler"
         >
-            TUTORIAL
+          TUTORIAL
         </Link>
-          <Link
-          to={'/practice'}
+        <Link
+          to="/practice"
           type="button"
           className="navbar__handler"
         >
-            PRACTICE
+          PRACTICE
         </Link>
-        </nav>
-      </header>
-    )
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
