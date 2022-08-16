42b4f8b190257c585702c49c8f49f3d994148b45
ulldecorb <ausias.bertran@gmail.com>
2022-08-16 17:24:35 +0200

    Add react-router and react-router-dom dependencies Create routes environment on app component

--- catalan-time/src/App.js
+++ catalan-time/src/App.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import './App.css';
 import Converter from './components/converter';
+import Tutorial from './components/tutorial';
+import Practice from './components/practice';
 
 function App() {
   const [ initialDate, setInitialDate ] = useState('')
@@ -15,13 +22,34 @@ function App() {
     setDate()
   },[])
   
-  return (
+  return (    
+      <Router>
     <div className="App">
       <header className="App__header">
         <h1 className='App__title'>CATALAN TIME</h1>
       </header>
-      <Converter initialDate={initialDate} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Converter initialDate={initialDate} />
+            }
+            />
+          <Route
+            path="/tutorial"
+            element={
+              <Tutorial />
+            }
+            />
+          <Route
+            path="/practice"
+            element={
+              <Practice />
+            }
+            />
+            </Routes>
     </div>
+      </Router>
   );
 }
 
---
+++ catalan-time/src/components/practice/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Practice () {
+    return(
+        <section className="practice">
+        <h2 className="practice__title">Practice</h2>
+        </section>
+    )
+}
+
+export default Practice;
