dd2c7eb052d8b1030b20735a1018fd0c80ba66e1
ulldecorb <ausias.bertran@gmail.com>
2022-09-11 02:46:50 +0200

    Create constants

---
+++ catalan-time/src/constants/index.js
@@ -0,0 +1,34 @@
+const answers = {
+  easyFromString: [
+    {
+      topics: {
+        statement: 'Dos quarts de set',
+        hourAnswer: 6,
+        minuteAnswer: 30
+      }
+    },
+    {
+      topics: {
+        statement: 'Les set en punt',
+        hourAnswer: 7,
+        minuteAnswer: 0
+      }
+    },
+    {
+      topics: {
+        statement: 'Un quart de deu',
+        hourAnswer: 9,
+        minuteAnswer: 15
+      }
+    },
+    {
+      topics: {
+        statement: 'Tres quarts de dues',
+        hourAnswer: 1,
+        minuteAnswer: 45
+      }
+    }
+  ]
+};
+
+export default answers;
